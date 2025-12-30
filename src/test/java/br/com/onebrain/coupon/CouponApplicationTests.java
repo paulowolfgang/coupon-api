f9c40eea2c3b400497775cddff15caa708d8f396
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-12-30 00:34:08 -0300

    Projeto configurado

---
+++ src/test/java/br/com/onebrain/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.onebrain.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
